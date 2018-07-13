15347ff1c4bf0e13be6c524d96f3d80cb9bf3001
Abdelkader Amar <abdelkader.amar@outlook.com>
2018-07-13 16:36:40 +0200

    Some tests

--- IEXStatsGUI/MainWindow.xaml.cs
+++ IEXStatsGUI/MainWindow.xaml.cs
@@ -13,28 +13,48 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Data;
+using System.Windows.Shapes;
+using System.Windows.Input;
 
 namespace IEXStatsGUI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : Window
     {
         private BackgroundWorker _worker;
+
+        public delegate void CellChangeEventHandler(object sender, CellChangeEventArgs e);
+
+        public event CellChangeEventHandler CellChanged;
 
         public MainWindow()
         {
             InitializeComponent();
 
+            CellChanged += new CellChangeEventHandler(OnCellChanged);
+
             OrderBook = new List<OrderBookLine>();
-            OrderBook.Add(new OrderBookLine());
-            OrderBook.Add(new OrderBookLine());
-            OrderBook.Add(new OrderBookLine());
-            OrderBook.Add(new OrderBookLine());
-            OrderBook.Add(new OrderBookLine());
+            int index = 0;
+            OrderBook.Add(new OrderBookLine(index++, CellChanged));
+            OrderBook.Add(new OrderBookLine(index++, CellChanged));
+            OrderBook.Add(new OrderBookLine(index++, CellChanged));
+            OrderBook.Add(new OrderBookLine(index++, CellChanged));
+            OrderBook.Add(new OrderBookLine(index++, CellChanged));
 
             DataContext = this;
+
 
             _worker = new BackgroundWorker();
             _worker.DoWork += worker_DoUpdate;
@@ -46,7 +66,7 @@ namespace IEXStatsGUI
             Random rand = new Random();
             while (true)
             {
-                Thread.Sleep(10);
+                Thread.Sleep(1000);
                 int row = rand.Next(0, OrderBook.Count);
                 int t = rand.Next(0, 6);
 
@@ -72,70 +92,167 @@ namespace IEXStatsGUI
                         break;
                 }
             }
+
+
+        }
+
+        Storyboard sb = new Storyboard();
+        private static DoubleAnimation blink = new DoubleAnimation()
+        {
+            From = 0,
+            To = 1,
+            Duration = TimeSpan.FromSeconds(0.2),
+            FillBehavior = FillBehavior.HoldEnd
+        };
+
+        private static ColorAnimation colorAnimation = new ColorAnimation(Colors.Blue, TimeSpan.FromSeconds(5), 
+            FillBehavior.HoldEnd);
+
+        protected virtual void OnCellChanged(object sender, CellChangeEventArgs e)
+        {
+            
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                sb.Stop();
+                sb.Children.Clear();
+                //sb.Children.Add(blink);
+                sb.Children.Add(colorAnimation);
+
+                DataGridCell cell = GetCell(e.Row, e.Column, dataGrid);
+
+                //Storyboard.SetTarget(blink, cell.Content as TextBlock);
+                //Storyboard.SetTargetProperty(blink, new PropertyPath(Button.OpacityProperty));
+
+                //Storyboard.SetTarget(colorAnimation, cell.Content as TextBlock);
+                Storyboard.SetTarget(colorAnimation, cell.Content as TextBlock);
+                Storyboard.SetTargetName(colorAnimation, "MySolidColorBrush");
+                Storyboard.SetTargetProperty(colorAnimation,
+                    new PropertyPath("Background.Color"));
+
+                sb.Begin();
+                //DataGridCell cell = GetCell(e.Row, e.Column, dataGrid);
+                //cell.Background = new SolidColorBrush(Colors.Red);
+            }));
         }
 
         public IList<OrderBookLine> OrderBook { get; set; }
 
-        public event PropertyChangedEventHandler PropertyChanged;
 
-        protected void OnPropertyChanged(string name)
+        public DataGridCell GetCell(int rowIndex, int columnIndex, DataGrid dg)
         {
-            PropertyChangedEventHandler handler = PropertyChanged;
-            if (handler != null)
-            {
-                handler(this, new PropertyChangedEventArgs(name));
-            }
+            DataGridRow row = dataGrid.ItemContainerGenerator.ContainerFromIndex(rowIndex) as DataGridRow;
+            DataGridCellsPresenter p = GetVisualChild<DataGridCellsPresenter>(row);
+            DataGridCell cell = p.ItemContainerGenerator.ContainerFromIndex(columnIndex) as DataGridCell;
+            return cell;
         }
 
+        static T GetVisualChild<T>(Visual parent) where T : Visual
+        {
+            T child = default(T);
+            int numVisuals = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < numVisuals; i++)
+            {
+                Visual v = (Visual)VisualTreeHelper.GetChild(parent, i);
+                child = v as T;
+                if (child == null)
+                {
+                    child = GetVisualChild<T>(v);
+                }
+                if (child != null)
+                {
+                    break;
+                }
+            }
+            return child;
+        }
         public class OrderBookLine : INotifyPropertyChanged
         {
             long _bidOrderCount, _bidSize, _askOrderCount, _askSize;
             decimal _bid, _ask;
+            CellChangeEventHandler _handler;
+
+            public static readonly int COL_BID_ORDER_COUNT = 0;
+            public static readonly int COL_BID_SIZE = 1;
+            public static readonly int COL_BID = 2;
+            public static readonly int COL_ASK_ORDER_COUNT = 3;
+            public static readonly int COL_ASK_SIZE = 4;
+            public static readonly int COL_ASK = 5;
+
+            public OrderBookLine(int row, CellChangeEventHandler handler)
+            {
+                Row = row;
+                _handler = handler;
+            }
+
+            public int Row { get; }
 
             public long BidOrderCount
             {
                 get { return _bidOrderCount; }
-                set { _bidOrderCount = value; OnPropertyChanged("BidOrderCount"); }
+                set { bool up = value > _bidOrderCount; _bidOrderCount = value; OnPropertyChanged("BidOrderCount", Row, COL_BID_ORDER_COUNT, up); }
             }
 
             public long BidSize
             {
                 get { return _bidSize; }
-                set { _bidSize = value; OnPropertyChanged("BidSize"); }
+                set { bool up = value > _bidSize; _bidSize = value; OnPropertyChanged("BidSize", Row, COL_BID_SIZE, up); }
             }
             public decimal Bid
             {
                 get { return _bid; }
-                set { _bid = value; OnPropertyChanged("Bid"); }
+                set { bool up = value > _bid; _bid = value; OnPropertyChanged("Bid", Row, COL_BID, up); }
             }
 
             public decimal Ask
             {
                 get { return _ask; }
-                set { _ask = value; OnPropertyChanged("Ask"); }
+                set { bool up = value > _ask; _ask = value; OnPropertyChanged("Ask", Row, COL_ASK, up); }
             }
 
             public long AskSize
             {
                 get { return _askSize; }
-                set { _askSize = value; OnPropertyChanged("AskSize"); }
+                set { bool up = value > _askSize; _askSize = value; OnPropertyChanged("AskSize", Row, COL_ASK_SIZE, up); }
             }
 
             public long AskOrderCount
             {
                 get { return _askOrderCount; }
-                set { _askOrderCount = value; OnPropertyChanged("AskOrderCount"); }
+                set { bool up = value > _askOrderCount; _askOrderCount = value; OnPropertyChanged("AskOrderCount", Row, COL_ASK_ORDER_COUNT, up); }
             }
 
             public event PropertyChangedEventHandler PropertyChanged;
 
-            protected void OnPropertyChanged(string name)
+            protected void OnPropertyChanged(string name, int row, int col, bool up)
             {
                 if (PropertyChanged != null)
                 {
                     PropertyChanged(this, new PropertyChangedEventArgs(name));
                 }
+                _handler(this, new CellChangeEventArgs(row, col, up));
             }
         }
+
+        private void dataGrid_TargetUpdated(object sender, DataTransferEventArgs e)
+        {
+            Console.WriteLine("Cell changed");
+
+        }
+    }
+
+    public class CellChangeEventArgs : EventArgs
+    {
+        public CellChangeEventArgs(int row, int col, bool up)
+        {
+            Row = row;
+            Column = col;
+            Up = up;
+        }
+
+        public int Row { get; }
+
+        public int Column { get; }
+
+        public bool Up { get; }
     }
 }
---
+++ Samples/Sandbox/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sandbox
+{
+    class OrderBookLine
+    {
+        public long BidSize { get; set; }
+        public decimal Bid { get; set; }
+        public decimal Ask { get; set; }
+        public long AskSize { get; set; }
+    }
+
+    public partial class Form1 : Form
+    {
+        List<OrderBookLine> _orderBook;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _orderBook = new List<OrderBookLine>() {
+    new OrderBookLine { BidSize = 10, Bid=9.9m, Ask=10.1m, AskSize = 20 },
+    new OrderBookLine { BidSize = 10, Bid=9.9m, Ask=10.1m, AskSize = 20 , },
+};
+            var bindingList = new BindingList<OrderBookLine>(_orderBook);
+            var source = new BindingSource(bindingList, null);
+            dataGridView1.DataSource = source;
+        }
+    }
+}
