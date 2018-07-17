97affa34f78e6134e7d1ddba5bc71c4da3e315da
Abdelkader Amar <abdelkader.amar@gmail.com>
2018-07-18 01:04:47 +0200

    Adding unit test project

---
+++ Tests/IexApiTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IexApiTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IexApiTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("52190500-6464-4380-a73f-af22d0021018")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tests/IexApiTests/TopDataTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using IEX.Api.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace IexApiTests
+{
+    [TestClass]
+    public class TopDataTest
+    {
+        [TestMethod]
+        public void FromJson()
+        {
+            var json = JsonConvert.DeserializeObject("[{\"symbol\":\"SNAP\",\"sector\":\"softwareservices\",\"securityType\":\"commonstock\",\"bidPrice\":0,\"bidSize\":0,\"askPrice\":0,\"askSize\":0,\"lastUpdated\":1531857600000,\"lastSalePrice\":13.405,\"lastSaleSize\":100,\"lastSaleTime\":1531857595826,\"volume\":217309,\"marketPercent\":0.01366},{\"symbol\":\"FB\",\"sector\":\"softwareservices\",\"securityType\":\"commonstock\",\"bidPrice\":0,\"bidSize\":0,\"askPrice\":0,\"askSize\":0,\"lastUpdated\":1531857600450,\"lastSalePrice\":209.99,\"lastSaleSize\":200,\"lastSaleTime\":1531857599424,\"volume\":297684,\"marketPercent\":0.01983},{\"symbol\":\"AIG+\",\"sector\":\"n/a\",\"securityType\":\"warrant\",\"bidPrice\":0,\"bidSize\":0,\"askPrice\":0,\"askSize\":0,\"lastUpdated\":1531857600000,\"lastSalePrice\":16.25,\"lastSaleSize\":200,\"lastSaleTime\":1531857591509,\"volume\":6400,\"marketPercent\":0.05478}]");
+            Assert.IsNotNull(json);
+            Assert.IsInstanceOfType(json, typeof(JArray));
+
+            JArray jarray = json as JArray;
+
+            List<JObject> list = new List<JObject>(jarray.Children<JObject>());
+            Assert.AreEqual(3, list.Count);
+
+            var snap = TopsData.FromJson(list[0]);
+            Assert.IsNotNull(snap);
+            Assert.AreEqual("SNAP", snap.Symbol);
+            Assert.AreEqual("softwareservices", snap.Sector);
+            Assert.AreEqual("commonstock", snap.SecurityType);
+            Assert.AreEqual(0m, snap.BidPrice);
+            Assert.AreEqual(0, snap.BidSize);
+            Assert.AreEqual(0m, snap.AskPrice);
+            Assert.AreEqual(0, snap.AskSize);
+            Assert.AreEqual(13.405m, snap.LastSalePrice);
+            Assert.AreEqual(100, snap.LastSaleSize);
+            Assert.AreEqual(217309, snap.Volume);
+            Assert.AreEqual(0.01366, snap.MarketPercent);
+        }
+    }
+}
