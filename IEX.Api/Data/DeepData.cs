fd25b4f1850f34394c20fa0a23e1b9c34388c7d6
Abdelkader Amar <abdelkader.amar@outlook.com>
2018-07-18 10:41:37 +0200

    Adding Deep data type

---
+++ IEX.Api/Data/DeepData.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace IEX.Api.Data
+{
+    /**
+     * 	
+    "symbol": "SNAP",
+	"marketPercent": 0,
+	"volume": 0,
+	"lastSalePrice": 0,
+	"lastSaleSize": 0,
+	"lastSaleTime": 0,
+	"lastUpdated": 0,
+	"bids": [],
+	"asks": [],
+	"systemEvent": {},
+    */
+    public class DeepData
+    {
+        #region JSON keys
+        public static readonly string SYMBOL_KEY = "symbol";
+        public static readonly string MARKET_PERCENT_KEY = "marketPercent";
+        public static readonly string VOLUME_KEY = "volume";
+        public static readonly string LAST_SALE_PRICE_KEY = "lastSalePrice";
+        public static readonly string LAST_SALE_SIZE_KEY = "lastSaleSize";
+        public static readonly string LAST_SALE_TIME_KEY = "lastSaleTime";
+        public static readonly string LAST_UPDATED_KEY = "lastUpdated";
+        public static readonly string BIDS_KEY = "bids";
+        public static readonly string ASKS_KEY = "asks";
+        public static readonly string SYSTEM_EVENT_KEY = "systemEvent";
+        public static readonly string TRADES_KEY = "trades";
+        public static readonly string TRADE_BREAKS_KEY = "tradeBreaks";
+        #endregion
+
+        public DeepData(string symbol)
+        {
+            Symbol = symbol;
+        }
+
+        #region Properties
+
+        public string Symbol { get; }
+
+        public double MarketPercent { get; set; }
+        
+        public long Volume { get; set; }
+
+        public decimal LastSalePrice { get; set; }
+
+        public long LastSaleSize { get; set; }
+
+        public DateTime LastSaleTime { get; set; }
+
+        public DateTime LastUpdated { get; set; }
+
+        // TODO bids, asks, systemEvent, trades and tradeBreaks
+        #endregion
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("Deep Data for {0}:", Symbol).Append(Environment.NewLine).
+                AppendFormat("\t- Market Percent = [{0}], Volume = [{1}]", MarketPercent, Volume).Append(Environment.NewLine).
+                AppendFormat("\t- Last : Sale Price = [{0}], Sale Size = [{1}], Sale Time = [{2}]", LastSalePrice, LastSaleSize, LastSaleTime).Append(Environment.NewLine).
+                AppendFormat("\t- Last Update = {0}", LastUpdated);
+            // TODO :  bids, asks, trades and tradeBreaks
+            return sb.ToString();
+        }
+
+        public static DeepData FromJson(JObject json)
+        {
+            string symbol = JsonHelper.GetValue(json, SYMBOL_KEY);
+            DeepData deepData = new DeepData(symbol)
+            {
+                MarketPercent = JsonHelper.GetDoubleValue(json, MARKET_PERCENT_KEY),
+                Volume = JsonHelper.GetLongValue(json, VOLUME_KEY),
+                LastSalePrice = JsonHelper.GetDecimalValue(json, LAST_SALE_PRICE_KEY),
+                LastSaleSize = JsonHelper.GetLongValue(json, LAST_SALE_SIZE_KEY),
+                LastSaleTime = JsonHelper.GetDateTimeValue(json, LAST_SALE_TIME_KEY),
+                LastUpdated = JsonHelper.GetDateTimeValue(json, LAST_UPDATED_KEY),
+            };
+            return deepData;
+        }
+    }
+}
---
+++ Tests/IexApiTests/DeepDataTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using IEX.Api.Data;
+
+namespace IexApiTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void FromJson()
+        {
+            var json = JsonConvert.DeserializeObject("{\"symbol\":\"SNAP\",\"marketPercent\":0,\"volume\":0,\"lastSalePrice\":0,\"lastSaleSize\":0,\"lastSaleTime\":0,\"lastUpdated\":0,\"bids\":[],\"asks\":[],\"systemEvent\":{},\"trades\":[{\"price\":13.405,\"size\":100,\"tradeId\":603927365,\"isISO\":false,\"isOddLot\":false,\"isOutsideRegularHours\":false,\"isSinglePriceCross\":false,\"isTradeThroughExempt\":false,\"timestamp\":1531857595826},{\"price\":13.4,\"size\":900,\"tradeId\":603235654,\"isISO\":true,\"isOddLot\":false,\"isOutsideRegularHours\":false,\"isSinglePriceCross\":false,\"isTradeThroughExempt\":false,\"timestamp\":1531857593460},{\"price\":13.4,\"size\":900,\"tradeId\":603227109,\"isISO\":false,\"isOddLot\":false,\"isOutsideRegularHours\":false,\"isSinglePriceCross\":false,\"isTradeThroughExempt\":false,\"timestamp\":1531857593420},{\"price\":13.4,\"size\":400,\"tradeId\":603226100,\"isISO\":false,\"isOddLot\":false,\"isOutsideRegularHours\":false,\"isSinglePriceCross\":false,\"isTradeThroughExempt\":false,\"timestamp\":1531857593412},{\"price\":13.4,\"size\":100,\"tradeId\":603226093,\"isISO\":false,\"isOddLot\":false,\"isOutsideRegularHours\":false,\"isSinglePriceCross\":false,\"isTradeThroughExempt\":false,\"timestamp\":1531857593412},{\"price\":13.4,\"size\":100,\"tradeId\":603226086,\"isISO\":false,\"isOddLot\":false,\"isOutsideRegularHours\":false,\"isSinglePriceCross\":false,\"isTradeThroughExempt\":false,\"timestamp\":1531857593412},{\"price\":13.4,\"size\":500,\"tradeId\":603225931,\"isISO\":false,\"isOddLot\":false,\"isOutsideRegularHours\":false,\"isSinglePriceCross\":false,\"isTradeThroughExempt\":false,\"timestamp\":1531857593411},{\"price\":13.4,\"size\":1072,\"tradeId\":603225882,\"isISO\":true,\"isOddLot\":false,\"isOutsideRegularHours\":false,\"isSinglePriceCross\":false,\"isTradeThroughExempt\":false,\"timestamp\":1531857593411},{\"price\":13.4,\"size\":700,\"tradeId\":603225846,\"isISO\":false,\"isOddLot\":false,\"isOutsideRegularHours\":false,\"isSinglePriceCross\":false,\"isTradeThroughExempt\":false,\"timestamp\":1531857593411},{\"price\":13.4,\"size\":628,\"tradeId\":603225748,\"isISO\":false,\"isOddLot\":false,\"isOutsideRegularHours\":false,\"isSinglePriceCross\":false,\"isTradeThroughExempt\":false,\"timestamp\":1531857593411},{\"price\":13.4,\"size\":100,\"tradeId\":603225738,\"isISO\":false,\"isOddLot\":false,\"isOutsideRegularHours\":false,\"isSinglePriceCross\":false,\"isTradeThroughExempt\":false,\"timestamp\":1531857593411},{\"price\":13.4,\"size\":1072,\"tradeId\":603225729,\"isISO\":false,\"isOddLot\":false,\"isOutsideRegularHours\":false,\"isSinglePriceCross\":false,\"isTradeThroughExempt\":false,\"timestamp\":1531857593411},{\"price\":13.395,\"size\":300,\"tradeId\":602933933,\"isISO\":false,\"isOddLot\":false,\"isOutsideRegularHours\":false,\"isSinglePriceCross\":false,\"isTradeThroughExempt\":false,\"timestamp\":1531857591175},{\"price\":13.395,\"size\":100,\"tradeId\":602933926,\"isISO\":false,\"isOddLot\":false,\"isOutsideRegularHours\":false,\"isSinglePriceCross\":false,\"isTradeThroughExempt\":false,\"timestamp\":1531857591175},{\"price\":13.4,\"size\":2028,\"tradeId\":602795983,\"isISO\":true,\"isOddLot\":false,\"isOutsideRegularHours\":false,\"isSinglePriceCross\":false,\"isTradeThroughExempt\":false,\"timestamp\":1531857590352},{\"price\":13.4,\"size\":900,\"tradeId\":602795961,\"isISO\":false,\"isOddLot\":false,\"isOutsideRegularHours\":false,\"isSinglePriceCross\":false,\"isTradeThroughExempt\":false,\"timestamp\":1531857590352},{\"price\":13.395,\"size\":100,\"tradeId\":600891507,\"isISO\":false,\"isOddLot\":false,\"isOutsideRegularHours\":false,\"isSinglePriceCross\":false,\"isTradeThroughExempt\":false,\"timestamp\":1531857579374},{\"price\":13.4,\"size\":100,\"tradeId\":600891020,\"isISO\":true,\"isOddLot\":false,\"isOutsideRegularHours\":false,\"isSinglePriceCross\":false,\"isTradeThroughExempt\":false,\"timestamp\":1531857579367},{\"price\":13.395,\"size\":100,\"tradeId\":599936095,\"isISO\":false,\"isOddLot\":false,\"isOutsideRegularHours\":false,\"isSinglePriceCross\":false,\"isTradeThroughExempt\":false,\"timestamp\":1531857573217},{\"price\":13.395,\"size\":100,\"tradeId\":599132909,\"isISO\":false,\"isOddLot\":false,\"isOutsideRegularHours\":false,\"isSinglePriceCross\":false,\"isTradeThroughExempt\":false,\"timestamp\":1531857569622}],\"tradeBreaks\":[]}");
+
+            Assert.IsNotNull(json);
+            Assert.IsInstanceOfType(json, typeof(JObject));
+
+            var deepData = DeepData.FromJson(json as JObject);
+            Assert.IsNotNull(deepData);
+
+            Assert.AreEqual("SNAP", deepData.Symbol);
+            Assert.AreEqual(0.0, deepData.MarketPercent);
+            Assert.AreEqual(0, deepData.Volume);
+            Assert.AreEqual(0m, deepData.LastSalePrice);
+            Assert.AreEqual(0, deepData.LastSaleSize);
+        }
+    }
+}
