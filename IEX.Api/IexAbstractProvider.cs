53ee191d990890a7a9cb9269e99c9c13b7dc3de2
Abdelkader Amar <abdelkader.amar@gmail.com>
2018-08-17 16:12:33 +0200

    Minor changes

--- IEX.Api/IexAbstractProvider.cs
+++ IEX.Api/IexAbstractProvider.cs
@@ -5,6 +5,8 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 
